b63d3dc7ac0caaffd26fcc29da7740d8e45f7210
Leo Qin <706046320@qq.com>
2018-01-22 15:32:31 +0800

    modify title

--- resources/views/public/head.blade.php
+++ resources/views/public/head.blade.php
@@ -4,17 +4,17 @@
     <meta name="description" content="">
     <meta name="keywords" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>BLOG index with sidebar & slider  | Amaze UI Examples</title>
+    <title>BLOG OF LEO QIN</title>
     <meta name="renderer" content="webkit">
     <meta http-equiv="Cache-Control" content="no-siteapp"/>
-    <link rel="icon" type="image/png" href="assets/i/favicon.png">
+    <link rel="icon" type="image/png" href="assets/i/blog.jpg">
     <meta name="mobile-web-app-capable" content="yes">
-    <link rel="icon" sizes="192x192" href="{{ asset('/assets/i/app-icon72x72@2x.png') }}">
+    <link rel="icon" sizes="192x192" href="{{ asset('/assets/i/blog.jpg') }}">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <meta name="apple-mobile-web-app-title" content="Amaze UI"/>
-    <link rel="apple-touch-icon-precomposed" href="{{ asset('/assets/i/app-icon72x72@2x.png') }}">
-    <meta name="msapplication-TileImage" content="{{ asset('/assets/i/app-icon72x72@2x.png') }}">
+    <meta name="apple-mobile-web-app-title" content="leo_qin"/>
+    <link rel="apple-touch-icon-precomposed" href="{{ asset('/assets/i/blog.jpg') }}">
+    <meta name="msapplication-TileImage" content="{{ asset('/assets/i/blog.jpg') }}">
     <meta name="msapplication-TileColor" content="#0e90d2">
     @include('public.css')
 </head>
