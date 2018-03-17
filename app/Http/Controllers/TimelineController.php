107140960ec8f842b5cb8ada9f4f800d425b0eb7
Leo Qin <706046320@qq.com>
2018-03-17 13:17:10 +0800

    add timeline

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -11,7 +11,7 @@ class TimelineController extends BaseController
     //
     public function index()
     {
-        $texts = DB::select("select id,date_format(updated_at,'%Y') year, date_format(updated_at,'%m') month,title,updated_at from texts  where is_show = 1 order by updated_at desc");
+        $texts = DB::select("select id,date_format(updated_at,'%Y') year, date_format(updated_at,'%m') month,title,date_format(updated_at,'%Y-%m-%d') updated_at from texts  where is_show = 1 order by updated_at desc");
         return response()->json($texts);
         // $texts = Texts::select('id','title','updated_at')->where('is_show',1)->orderBy('updated_at','desc')->get();
         foreach ($texts as $val) {
