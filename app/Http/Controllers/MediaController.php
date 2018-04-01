ca045a4abdff38c64f54a6ab6325293995d36d5c
Leo Qin <706046320@qq.com>
2018-04-01 22:42:05 +0800

    rm _id from media

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -10,7 +10,7 @@ class MediaController extends BaseController
     //
     public function index(Request $request)
     {
-        $category_id = $request->get('category_id');
+        $category_id = $request->get('category');
         $media = Images::when(!empty($category_id),function($query) use ($category_id) {
             return $query->where('category_id',$category_id);
         })->where('is_show','1')->get();
