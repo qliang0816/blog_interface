2d4e02c1c826ff454e6af6cf8cdc9ff05b50d26c
Leo Qin <706046320@qq.com>
2018-04-03 23:12:33 +0800

    tag category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Texts;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends BaseController
 {
@@ -19,13 +20,32 @@ class HomeController extends BaseController
         $category_id = $request->input('category_id');
         // 搜索
         $searchData = $request->input('searchData');
-        $texts = Texts::select('id','title','summary','category_id','image','updated_at')->where('is_show','1')
-        ->when(!empty($category_id),function($query) use ($category_id){
-                return $query->where('category_id',$category_id);
+        // 标签
+        $tag_id = $request->input('tag_id');
+        if (!empty($tag_id)) {
+            $tags = DB::table('text_tags')->where('tag_id', $tag_id)->get();
+            if (!$tags->isEmpty()) {
+                for ($i = 0; $i < count($tags); $i++) {
+                    $in_query[] = $tags[$i]->text_id;
+                }
+            } else {
+                $in_query = [];
+            }
+        } else {
+            $in_query = [];
+        }
+        
+        $texts = Texts::select('id', 'title', 'summary', 'category_id', 'image', 'updated_at')
+            ->where('is_show', '1')
+            ->whereIn('id', $in_query)
+            ->when(!empty($category_id), function ($query) use ($category_id) {
+                return $query->where('category_id', $category_id);
             })
-        ->when(!empty($searchData),function($query) use ($searchData){
-                return $query->where('title','like','%'.$searchData.'%');
-            })->orderBy('updated_at','desc')->paginate($paginate);
+            ->when(!empty($searchData), function ($query) use ($searchData) {
+                return $query->where('title', 'like', '%' . $searchData . '%');
+            })
+            ->orderBy('updated_at', 'desc')
+            ->paginate($paginate);
         return response()->json($texts);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,27 @@
 
 // 首页文章分页
 Route::get('/home', "HomeController@home");
+
 // 文章标题分类
 Route::get('/category', "CategoryController@category");
+
 // 标签分类
 Route::get('/tagcate', "CategoryController@tagCate");
+
 // 文章标题分类
 Route::get('/titlecate', "CategoryController@titleCate");
+
 // 图片分类
 Route::get('/imagecate', "CategoryController@imageCate");
+
 // 文章内容和前后两篇文章
 Route::get('/article', 'ArticleController@index');
+
 // 时间线
 Route::get('/timeline', 'TimelineController@index');
+
 // 图片
 Route::get('/media', 'MediaController@index');
-
 
 
 Route::group(['prefix' => 'bk'], function () {
