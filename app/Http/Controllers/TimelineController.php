d7b453c68defc7e61fa5da858836b91e749f2410
Leo Qin <706046320@qq.com>
2018-04-13 17:59:29 +0800

    modify timeline add tags

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -10,7 +10,24 @@ class TimelineController extends BaseController
     //
     public function index()
     {
-        $texts = DB::select("select id,date_format(updated_at,'%Y') year, date_format(updated_at,'%m') month,title,date_format(updated_at,'%Y-%m-%d') updated_at from texts  where is_show = 1 order by updated_at desc");
+        $texts = DB::select("
+        SELECT
+            texts.id ,
+            date_format(texts.updated_at , '%Y') year ,
+            date_format(texts.updated_at , '%m') month ,
+            title ,
+            date_format(texts.updated_at , '%Y-%m-%d') updated_at, 
+            GROUP_CONCAT(tags.`name`) tags,
+            GROUP_CONCAT(tags.`id`) tags_id
+        FROM
+            texts
+        LEFT JOIN text_tags ON texts.id = text_tags.text_id
+        LEFT JOIN tags ON tags.id = text_tags.tag_id
+        WHERE
+            is_show = 1
+        group by id,year,month,title,updated_at
+        ORDER BY
+        updated_at DESC;");
         return response()->json($texts);
     }
 }
